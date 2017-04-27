80ba2425e496362669df00bcefe758cad46d5788
ddtung <ddtung@apcs.vn>
2017-04-27 13:25:48 +0700

    update spinner

--- TodoList/app/src/main/java/com/example/tungd/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/example/tungd/todolist/MainActivity.java
@@ -4,16 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Spinner statusSpinner = (Spinner) findViewById(R.id.status);
+        Spinner prioritySpinner = (Spinner) findViewById(R.id.priority);
+        statusSpinner.setOnItemSelectedListener(this);
+        prioritySpinner.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -33,5 +39,26 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return false;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Spinner spinner = (Spinner) parent;
+        switch (spinner.getId()) {
+            case R.id.status:
+//                update list when select status
+                Toast.makeText(getApplicationContext(), spinner.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.priority:
+//                updaet list when select priority
+                Toast.makeText(getApplicationContext(), spinner.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
